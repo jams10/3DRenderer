d315a2eed19aac63f80228b40163d8bb148f9c27
jams10 <jamin3710@gmail.com>
2023-06-29 22:01:34 +0900

    034-2. Cube Mapping pixel shader comment.

--- Renderer/Renderer/Shaders/DrawingCubeMapPixelShader.hlsl
+++ Renderer/Renderer/Shaders/DrawingCubeMapPixelShader.hlsl
@@ -4,5 +4,8 @@ TextureCube g_textureCube0 : register(t0);
 
 float4 main(PixelShaderInput input) : SV_TARGET
 {
+    // 큐브맵은 원점에 위치하고 전체 씬을 감싸는 커다란 정육면체임.
+    // 따라서 큐브맵 텍스쳐를 샘플링 할 때는 월드 좌표 원점을 중심으로 정육 면체를 구성하는 각 정점의 위치를 기준으로 샘플링 해주면 됨.
+    // 월드 좌표가 샘플링 텍스쳐 좌표이므로 float3 타입.
     return g_textureCube0.Sample(linearClampSampler, input.posWorld.xyz);
 }
